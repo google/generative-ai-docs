0a82683357f34d958cebcd24af8efcdbc5ae8559
Kyo Lee <kyol@google.com>
2023-10-06 20:47:25 +0000

    [Release] version 0.1.2
    What's changed:
    
    - Refactored the `chatbot/chatui.py` to consolidate methods into `ask_model()`.
    - Updated the element names in the `result.html` template.
    - Cleaned up the code related to reading and replacing custom error messages.
    - Added more comments describing each prompt in the `ask_model()` method.
    - Moved the role of `condition.txt` to `config.yaml`.
    - Updated the Docs Agent module to read custom prompt text from `config.yaml`.
    - Removed the `condition.txt` file.
    - Re-generated the `poetry.lock` file to be up to date.
    
    October 6, 2023

--- demos/palm/python/docs-agent/chatbot/chatui.py
+++ demos/palm/python/docs-agent/chatbot/chatui.py
@@ -112,150 +112,113 @@ def rewrite():
         return redirect(url_for("chatui.index"))
 
 
+# Render a response page when the user asks a question
+# using input text box.
 @bp.route("/result", methods=["GET", "POST"])
 def result():
     if request.method == "POST":
-        uuid_value = uuid.uuid1()
-        question_captured = request.form["question"]
-        query_result = docs_agent.query_vector_store(question_captured)
-        context = markdown.markdown(query_result.fetch_formatted(Format.CONTEXT))
-        context_with_prefix = docs_agent.add_instruction_to_context(context)
-        response_in_markdown = docs_agent.ask_text_model_with_context(
-            context_with_prefix, question_captured
-        )
-        if response_in_markdown is None:
-            response_in_markdown = (
-                "The PaLM API is not able to answer this question at the moment. "
-                "Try to rephrase the question and ask again."
-            )
-        response_in_html = markdown.markdown(response_in_markdown)
-        metadatas = markdown.markdown(
-            query_result.fetch_formatted(Format.CLICKABLE_URL)
-        )
-        fact_checked_answer_in_markdown = docs_agent.ask_text_model_to_fact_check(
-            context_with_prefix, response_in_markdown
-        )
-        if fact_checked_answer_in_markdown is None:
-            fact_checked_answer_in_markdown = (
-                "The PaLM API is not able to answer this question at the moment. "
-                "Try to rephrase the question and ask again."
-            )
-        fact_checked_answer_in_html = markdown.markdown(fact_checked_answer_in_markdown)
-        new_question = (
-            "What are 5 questions developers might ask after reading the context?"
-        )
-        related_questions = markdown.markdown(
-            docs_agent.ask_text_model_with_context(response_in_markdown, new_question)
-        )
-        soup = BeautifulSoup(related_questions, "html.parser")
-        for item in soup.find_all("li"):
-            if item.string is not None:
-                link = soup.new_tag(
-                    "a",
-                    href=url_for(
-                        "chatui.question", ask=urllib.parse.quote_plus(item.string)
-                    ),
-                )
-                link.string = item.string
-                item.string = ""
-                item.append(link)
-        related_questions = soup
-        fact_link = markdown.markdown(
-            query_result.fetch_nearest_formatted(Format.CLICKABLE_URL)
-        )
-        server_url = request.url_root.replace("http", "https")
-        # Log the question and response to the log file.
-        log_question(uuid_value, question_captured, response_in_markdown)
-        return render_template(
-            "chatui/index.html",
-            question=question_captured,
-            context=context,
-            context_with_prefix=context_with_prefix,
-            response_in_markdown=response_in_markdown,
-            response_in_html=response_in_html,
-            product=product,
-            metadatas=metadatas,
-            fact_checked_answer=fact_checked_answer_in_html,
-            fact_link=fact_link,
-            related_questions=related_questions,
-            server_url=server_url,
-            uuid=uuid_value,
-        )
+        question = request.form["question"]
+        return ask_model(question)
     else:
         return redirect(url_for("chatui.index"))
 
 
+# Render a response page when the user clicks a question
+# from the related questions list.
 @bp.route("/question/<ask>", methods=["GET", "POST"])
 def question(ask):
     if request.method == "GET":
-        uuid_value = uuid.uuid1()
-        question_captured = urllib.parse.unquote_plus(ask)
-        query_result = docs_agent.query_vector_store(question_captured)
-        context = markdown.markdown(query_result.fetch_formatted(Format.CONTEXT))
-        context_with_prefix = docs_agent.add_instruction_to_context(context)
-        response_in_markdown = docs_agent.ask_text_model_with_context(
-            context_with_prefix, question_captured
-        )
-        if response_in_markdown is None:
-            response_in_markdown = (
-                "The PaLM API is not able to answer this question at the moment. "
-                "Try to rephrase the question and ask again."
-            )
-        response_in_html = markdown.markdown(response_in_markdown)
-        metadatas = markdown.markdown(
-            query_result.fetch_formatted(Format.CLICKABLE_URL)
-        )
-        fact_checked_answer_in_markdown = docs_agent.ask_text_model_to_fact_check(
-            context_with_prefix, response_in_markdown
-        )
-        if fact_checked_answer_in_markdown is None:
-            fact_checked_answer_in_markdown = (
-                "The PaLM API is not able to answer this question at the moment. "
-                "Try to rephrase the question and ask again."
-            )
-        fact_checked_answer_in_html = markdown.markdown(fact_checked_answer_in_markdown)
-        new_question = (
-            "What are 5 questions developers might ask after reading the context?"
-        )
-        related_questions = markdown.markdown(
-            docs_agent.ask_text_model_with_context(response_in_markdown, new_question)
-        )
-        soup = BeautifulSoup(related_questions, "html.parser")
-        for item in soup.find_all("li"):
-            if item.string is not None:
-                link = soup.new_tag(
-                    "a",
-                    href=url_for(
-                        "chatui.question", ask=urllib.parse.quote_plus(item.string)
-                    ),
-                )
-                link.string = item.string
-                item.string = ""
-                item.append(link)
-        related_questions = soup
-        fact_link = markdown.markdown(
-            query_result.fetch_nearest_formatted(Format.CLICKABLE_URL)
-        )
-        server_url = request.url_root.replace("http", "https")
-        # Log the question and response to the log file.
-        log_question(uuid_value, question_captured, response_in_markdown)
-        return render_template(
-            "chatui/index.html",
-            question=question_captured,
-            context=context,
-            context_with_prefix=context_with_prefix,
-            response_in_markdown=response_in_markdown,
-            response_in_html=response_in_html,
-            product=product,
-            metadatas=metadatas,
-            fact_checked_answer=fact_checked_answer_in_html,
-            fact_link=fact_link,
-            related_questions=related_questions,
-            server_url=server_url,
-            uuid=uuid_value,
-        )
+        question = urllib.parse.unquote_plus(ask)
+        return ask_model(question)
     else:
         return redirect(url_for("chatui.index"))
+
+
+# Construct a set of prompts using the user question, send the prompts to
+# the lanaguage model, receive responses, and present them into a page.
+def ask_model(question):
+    ### PROMPT 1: AUGMENT THE USER QUESTION WITH CONTEXT.
+    # 1. Use the question to retrieve a list of related contents from the database.
+    # 2. Convert the list of related contents into plain Markdown text (context).
+    # 3. Add the custom condition text to the context.
+    # 4. Send the prompt (condition + context + question) to the language model.
+    query_result = docs_agent.query_vector_store(question)
+    context = markdown.markdown(query_result.fetch_formatted(Format.CONTEXT))
+    context_with_prefix = docs_agent.add_instruction_to_context(context)
+    response = docs_agent.ask_text_model_with_context(context_with_prefix, question)
+
+    ### PROMPT 2: FACT-CHECK THE PREVIOUS RESPONSE.
+    fact_checked_response = docs_agent.ask_text_model_to_fact_check(
+        context_with_prefix, response
+    )
+
+    ### PROMPT 3: GET 5 RELATED QUESTIONS.
+    # 1. Prepare a new question asking the model to come up with 5 related questions.
+    # 2. Ask the language model with the new question.
+    # 3. Parse the model's response into a list in HTML format.
+    new_question = (
+        "What are 5 questions developers might ask after reading the context?"
+    )
+    new_response = markdown.markdown(
+        docs_agent.ask_text_model_with_context(response, new_question)
+    )
+    related_questions = parse_related_questions_response_to_html_list(new_response)
+
+    ### RETRIEVE SOURCE URLS.
+    # - Construct clickable URLs using the returned related contents above.
+    # - Extract the URL of the top related content for the fact-check message.
+    clickable_urls = markdown.markdown(
+        query_result.fetch_formatted(Format.CLICKABLE_URL)
+    )
+    fact_check_url = markdown.markdown(
+        query_result.fetch_nearest_formatted(Format.CLICKABLE_URL)
+    )
+
+    ### PREPARE OTHER ELEMENTS NEEDED BY UI.
+    # - Create a uuid for this request.
+    # - Convert the first response from the model into HTML for rendering.
+    # - Convert the fact-check response from the model into HTML for rendering.
+    # - A workaround to get the server's URL to work with the rewrite and like features.
+    new_uuid = uuid.uuid1()
+    response_in_html = markdown.markdown(response)
+    fact_checked_response_in_html = markdown.markdown(fact_checked_response)
+    server_url = request.url_root.replace("http", "https")
+
+    ### LOG THIS REQUEST.
+    log_question(new_uuid, question, response)
+
+    return render_template(
+        "chatui/index.html",
+        question=question,
+        context=context,
+        response=response,
+        response_in_html=response_in_html,
+        clickable_urls=clickable_urls,
+        fact_checked_response_in_html=fact_checked_response_in_html,
+        fact_check_url=fact_check_url,
+        related_questions=related_questions,
+        product=product,
+        server_url=server_url,
+        uuid=new_uuid,
+    )
+
+
+# Parse a response containing a list of related questions from the language model
+# and convert it into an HTML-based list.
+def parse_related_questions_response_to_html_list(response):
+    soup = BeautifulSoup(response, "html.parser")
+    for item in soup.find_all("li"):
+        if item.string is not None:
+            link = soup.new_tag(
+                "a",
+                href=url_for(
+                    "chatui.question", ask=urllib.parse.quote_plus(item.string)
+                ),
+            )
+            link.string = item.string
+            item.string = ""
+            item.append(link)
+    return soup
 
 
 # Log the question and response to the server's log file.
