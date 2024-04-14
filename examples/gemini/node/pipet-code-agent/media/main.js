ef5999b53d1f546f03b4444f8a044d121fb73845
Jason <5120011@qq.com>
2024-04-15 03:39:51 +0800

    Add clearChat; Add css & js in media; Add code highlight; sendMessageStream() use try catch error

---
+++ examples/gemini/node/pipet-code-agent/media/main.js
@@ -0,0 +1,109 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const vscode = acquireVsCodeApi();
+  const oldState = vscode.getState() || {};
+  let oldHtml = oldState || "<div></div>";
+
+  // 在这里执行获取元素的操作
+  const dialog = document.getElementById("dialog");
+  const userInput = document.getElementById("userInput");
+  const sendMessageButton = document.getElementById("sendMessage");
+  let chatID;
+  dialog.innerHTML = oldHtml;
+
+  function adjustTextareaHeight() {
+    userInput.style.height = "auto";
+    userInput.style.height = userInput.scrollHeight + "px";
+    sendMessageButton.disabled = !userInput.value;
+  }
+  userInput.addEventListener("input", adjustTextareaHeight);
+
+  // 显示接收到的Gemini消息
+  function showAIMessage(text, chatID) {
+    const messageContainer = document.createElement("div");
+    messageContainer.id = chatID;
+    messageContainer.classList.add("message-container");
+    messageContainer.innerHTML =
+      "<p><strong>Gemini:</strong> " + marked.parse(text) + "</p>";
+    dialog.appendChild(messageContainer);
+  }
+
+  function updateAIMessage(newText, chatID) {
+    const messageContainer = document.getElementById(chatID);
+
+    (messageContainer.innerHTML =
+      "<p><strong>Gemini:</strong> " + marked.parse(newText)),
+      +"</p>";
+    addBottons(messageContainer);
+    sendMessageButton.innerHTML = "发送";
+  }
+
+  // 显示问题
+  function showUserMessage(text) {
+    const messageContainer = document.createElement("div");
+    messageContainer.classList.add("message-container");
+    messageContainer.innerHTML =
+      "<p><strong>You:</strong> " + marked.parse(text) + "</p>";
+    addBottons(messageContainer);
+    dialog.appendChild(messageContainer);
+  }
+  /**
+   *
+   * @param {HTMLDivElement}
+   */
+  function addBottons(container) {
+    const svg =
+      '<svg width="16" height="16" xmlns="http://www.w3.org/2000/svg"> <g id="Layer_1">  <title>Layer 1</title>  <path stroke="null" id="svg_1" d="m11.17086,13.7471l-7.75098,0l0,-10.05744l-1.40928,0l0,10.05744c0,0.79023 0.63418,1.43678 1.40928,1.43678l7.75098,0l0,-1.43678zm2.81854,-2.87355l0,-8.62066c0,-0.79023 -0.63418,-1.43678 -1.40928,-1.43678l-6.34171,0c-0.7751,0 -1.40928,0.64655 -1.40928,1.43678l0,8.62066c0,0.79023 0.63418,1.43678 1.40928,1.43678l6.34171,0c0.7751,0 1.40928,-0.64655 1.40928,-1.43678zm-1.40928,0l-6.34171,0c0,-2.87355 0,-5.74711 0,-8.62066l6.34171,0l0,8.62066z" fill="#bfbdb6"/> </g></svg>';
+    const codeElements = container.querySelectorAll("pre code");
+    codeElements.forEach((codeElement) => {
+      const copyButton = document.createElement("button");
+      copyButton.innerHTML = svg;
+      copyButton.classList.add("code-buttons-container");
+      copyButton.addEventListener("click", () => {
+        const textToCopy = codeElement.textContent;
+        navigator.clipboard
+          .writeText(textToCopy)
+          .then(() => {
+            copyButton.innerHTML = svg;
+          })
+          .catch((error) => {
+            copyButton.innerHTML = "Failed";
+          });
+      });
+      codeElement.parentElement.classList.add("pre");
+      codeElement.parentElement.appendChild(copyButton);
+      Prism.highlightElement(codeElement);
+    });
+  }
+
+  // 当发送按钮被点击时
+  sendMessageButton.addEventListener("click", () => {
+    this.chatID = Math.random().toString();
+    const userMessage = userInput.value;
+    vscode.postMessage({ command: "sendMessage", text: userMessage });
+    userInput.value = "";
+    userInput.style.height = "auto";
+    sendMessageButton.disabled = true;
+    sendMessageButton.innerHTML = "Thinking...";
+  });
+
+  // 接收来自插件的消息
+  window.addEventListener("message", (event) => {
+    const message = event.data;
+    switch (message.command) {
+      case "receiveMessage":
+        updateAIMessage(message.text, this.chatID);
+        break;
+      case "Message":
+        showUserMessage(message.text);
+        showAIMessage("Thinking ...", this.chatID);
+        break;
+      case "clearChat":
+        dialog.innerHTML = "<div></div>";
+        break;
+      default:
+        console.log("Unknown command: " + message.command);
+        break;
+    }
+    vscode.setState(dialog.innerHTML);
+  });
+});
--- examples/gemini/node/pipet-code-agent/src/extension.ts
+++ examples/gemini/node/pipet-code-agent/src/extension.ts
@@ -18,6 +18,8 @@ import * as vscode from "vscode";
 import { generateComment } from "./comments";
 import { generateReview } from "./review";
 import { startchat } from "./chat";
+import { text } from "stream/consumers";
+import { toUSVString } from "util";
 
 export function activate(context: vscode.ExtensionContext) {
   vscode.commands.registerCommand(
@@ -30,16 +32,20 @@ export function activate(context: vscode.ExtensionContext) {
   );
 
   const provider = new ChatViewProvider(context.extensionUri);
-
   context.subscriptions.push(
     vscode.window.registerWebviewViewProvider(
       ChatViewProvider.viewType,
       provider
     )
   );
+  context.subscriptions.push(
+    vscode.commands.registerCommand("pipet-code-agent.clearChat", () => {
+      provider.clearChat();
+    })
+  );
 }
 class ChatViewProvider implements vscode.WebviewViewProvider {
-  public static readonly viewType = "pipet-code-agent.ChatView";
+  public static readonly viewType = "pipet-code-agent.chatView";
 
   private _view?: vscode.WebviewView;
 
@@ -58,184 +64,101 @@ class ChatViewProvider implements vscode.WebviewViewProvider {
       localResourceRoots: [this._extensionUri],
     };
 
-    webviewView.webview.html = this._getHtmlForWebview();
+    webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
 
-    const chat = startchat();
-
-    webviewView.webview.onDidReceiveMessage(async (message) => {
-      // 处理来自 Webview 的消息
-      if (message.command === "sendMessage" && message.text) {
-        // 在 Webview 中显示接收到的消息
-        webviewView.webview.postMessage({
-          command: "Message",
-          text: message.text,
-        });
-        if (chat) {
-          const result = await chat.sendMessageStream(message.text);
-          let chunktext = "";
-          for await (const chunk of result.stream) {
-            chunktext += chunk.text();
+    try {
+      const chat = startchat();
+      webviewView.webview.onDidReceiveMessage(async (message) => {
+        // 处理来自 Webview 的消息
+        if (message.command === "sendMessage" && message.text) {
+          // 在 Webview 中显示接收到的消息
+          webviewView.webview.postMessage({
+            command: "Message",
+            text: message.text,
+          });
+          if (chat) {
+            try {
+              const result = await chat.sendMessageStream(message.text);
+              let chunktext = "";
+              for await (const chunk of result.stream) {
+                chunktext += chunk.text();
+                webviewView.webview.postMessage({
+                  command: "receiveMessage",
+                  text: chunktext,
+                });
+              }
+            } catch (error) {
+              webviewView.webview.postMessage({
+                command: "receiveMessage",
+                text: `${error}`,
+              });
+            }
+          } else {
             webviewView.webview.postMessage({
               command: "receiveMessage",
-              text: chunktext,
+              text: "Chat Start Failed.",
             });
           }
-        } else {
-          webviewView.webview.postMessage({
-            command: "receiveMessage",
-            text: "请求失败。",
-          });
         }
-      }
-    });
+      });
+    } catch (error) {
+      webviewView.webview.postMessage({
+        command: "receiveMessage",
+        text: `${error}`,
+      });
+    }
   }
-
-  private _getHtmlForWebview() {
+  public clearChat() {
+    if (this._view) {
+      this._view.webview.postMessage({
+        command: "clearChat",
+      });
+    }
+  }
+  private _getHtmlForWebview(webview: vscode.Webview) {
+    const scriptUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "main.js")
+    );
+    const styleMainUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "main.css")
+    );
+    const styleCodeUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "prism.css")
+    );
+    // Use a nonce to only allow a specific script to be run.
+    const nonce = getNonce();
     return `<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Gemini Chat</title>
-    <style>
-      body {
-        margin: 0;
-        padding: 0;
-        display: flex;
-        flex-direction: column;
-        min-height: 100vh; /* 使内容占据整个视口高度 */
-      }
-
-      main {
-        flex: 1; /* 使 main 元素占据剩余的垂直空间 */
-        padding: 20px; /* 添加内边距 */
-      }
-
-      #dialog {
-        overflow-y: auto; /* 当内容过多时，显示滚动条 */
-        border-radius: 5px; /* 添加边框圆角 */
-        padding: 10px; /* 添加内边距 */
-        margin-bottom: 10px; /* 添加底部外边距 */
-      }
-
-      footer {
-        display: flex;
-        align-items: center;
-        padding: 10px;
-      }
-
-      #userInput {
-        height: auto;
-        resize: none;
-        background-color: var(
-          --vscode-input-background
-        ); /* 使用 VS Code 的输入框背景色 */
-        color: var(--vscode-input-foreground); /* 使用 VS Code 的输入框前景色 */
-        border: 1px solid var(--vscode-input-foreground); /* 使用 VS Code 的输入框前景色作为边框颜色 */
-        flex: 1;
-      }
-
-      #sendMessage {
-        background-color: var(
-          --vscode-input-background
-        ); /* 使用 VS Code 的输入框背景色 */
-        color: var(--vscode-input-foreground); /* 使用 VS Code 的输入框前景色 */
-        border: 1px solid var(--vscode-input-foreground); /* 使用 VS Code 的输入框前景色作为边框颜色 */
-        margin-left: 5px;        
-      }
-
-      .ai-message-container {
-        background-color: var(--vscode-input-background);
-        color: var(--vscode-editor-foreground);
-      }
-      .user-message-container {
-        background-color: var(--vscode-input-background);
-        color: var(--vscode-editor-foreground);
-      }
-    </style>
-  </head>
-  <body>
-    <main>
-      <h1>Gemini Chat</h1>
-      <div id="dialog"></div>
-    </main>
-    <footer>
-      <textarea id="userInput" placeholder="请输入您的消息"></textarea>
-      <button id="sendMessage" disabled>发送</button>
-    </footer>
-    <script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
-    <script>
-      const vscode = acquireVsCodeApi();
-
-      // 获取 UI 元素
-      const dialog = document.getElementById("dialog");
-      const userInput = document.getElementById("userInput");
-      const sendMessageButton = document.getElementById("sendMessage");
-      const ID = "";
-
-      function adjustTextareaHeight() {
-        userInput.style.height = "auto";
-        userInput.style.height = userInput.scrollHeight + "px";
-        sendMessageButton.disabled = !userInput.value;
-      }
-      userInput.addEventListener("input", adjustTextareaHeight);
-
-      // 显示接收到的Gemini消息
-      function showAIMessage(text, ID) {
-        const messageContainer = document.createElement("div");
-        messageContainer.id = ID;
-        messageContainer.classList.add("ai-message-container");
-        messageContainer.innerHTML =
-          "<p><strong>Gemini:</strong> " + marked.parse(text) + "</p>";
-        dialog.appendChild(messageContainer);
-      }
-
-      function updateAIMessage(newText, ID) {
-        const messageContainer = document.getElementById(ID);
-        messageContainer.innerHTML = "<p><strong>Gemini:</strong> " + marked.parse(newText) + "</p>";
-        sendMessageButton.innerHTML = "发送";
-      }
-
-      // 显示问题
-      function showUserMessage(text) {
-        const messageContainer = document.createElement("div");
-        messageContainer.classList.add("user-message-container");
-        messageContainer.innerHTML =
-          "<p><strong>User:</strong> " + marked.parse(text) + "</p>";
-        dialog.appendChild(messageContainer);
-      }
-
-      // 当发送按钮被点击时
-      sendMessageButton.addEventListener("click", () => {
-        this.ID = Math.random().toString();
-        const userMessage = userInput.value;
-        vscode.postMessage({ command: "sendMessage", text: userMessage });
-        userInput.value = "";
-        userInput.style.height = "auto";
-        sendMessageButton.disabled = true;
-        sendMessageButton.innerHTML = "Thinking...";
-      });
-
-      // 接收来自插件的消息
-      window.addEventListener("message", (event) => {
-        const message = event.data;
-        switch (message.command) {
-          case "receiveMessage":
-            updateAIMessage(message.text, this.ID);
-            break;
-          case "Message":
-            showUserMessage(message.text);
-            showAIMessage("Thinking ...", this.ID);
-            break;
-          default:
-            console.log("Unknown command: " + message.command);
-        }
-      });
-    </script>
-  </body>
-</html>
-`;
+            <html lang="en">
+              <head>
+                <meta charset="UTF-8" />
+                <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link href="${styleMainUri}" rel="stylesheet">
+                <link rel="stylesheet" href="${styleCodeUri}">
+                <title>Gemini Chat</title>
+              </head>
+              <body>
+                <main>
+                  <div id="dialog"></div>
+                </main>
+                <footer>
+                  <textarea id="userInput" placeholder="请输入您的消息"></textarea>
+                  <button id="sendMessage" disabled>发送</button>
+                </footer>
+                <script nonce="${nonce}" src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
+                <script nonce="${nonce}" src="https://cdn.jsdelivr.net/npm/prismjs/prism.js"></script>
+                <script nonce="${nonce}" src="${scriptUri}"></script>
+              </body>
+            </html>`;
   }
 }
-
+function getNonce() {
+  let text = "";
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < 32; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
 export function deactivate() {}
