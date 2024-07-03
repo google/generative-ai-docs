a31ad3d50bca7ad346378bcad5835d9e9d7eec42
Mark Daoust <markdaoust@google.com>
2024-07-03 09:40:59 -0700

    These have moved to the generative-ai-python repo

--- examples/gemini/python/api/generatecontent_text.py
+++
@@ -1,9 +0,0 @@
-import os
-import google.generativeai as genai
-
-genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
-
-model = genai.GenerativeModel("gemini-1.5-flash")
-
-response = model.generate_content("Give me python code to sort a list")
-print(response.text)
